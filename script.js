6fa4254fbbe55170bd778dab99de78ec87f25955
Wempye Johan Moen Waoma <167386883+wempywaoma@users.noreply.github.com>
2024-08-14 09:57:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,15 @@ $(document).ready(function () {
     },
     {
       title: "I Have Crush On You!!",
-      desc: "mungkin itu sebuah kalimat yg cukup simple, 
-        kalimat yang menyimpan banyak harapan juga keraguan dibaliknya. 
-    ada harapan yg ingin rasa suka itu tidak jatuh sendirian, 
-    juga ada yg harus disiapkan agar bisa menerima konsekuensinya.",
+      desc: "mungkin itu sebuah kalimat yg cukup simple, kalimat yang menyimpan banyak harapan juga keraguan dibaliknya. ada harapan yg ingin rasa suka itu tidak jatuh sendirian, juga ada yg harus disiapkan agar bisa menerima konsekuensinya.",
     },
     {
       title: "",
-      desc: "menyatakan perasaan itu hal yg cukup serius, 
-        banyak yg akan dikorbankan termasuk hasil akhirnya melepas hal yg sudah Iama tersimpan itu cukup melegakan dan bukan hal yg mudah. 
-        Disini Aku cuman mau ungkapin perasaan aku sama kamu, 
-  masalah diterima atau ngga itu tergantung kamu nya, 
-  aku juga gak bakal berharap lebih kok",
+      desc: "menyatakan perasaan itu hal yg cukup serius, banyak yg akan dikorbankan termasuk hasil akhirnya melepas hal yg sudah Iama tersimpan itu cukup melegakan dan bukan hal yg mudah. Disini Aku cuman mau ungkapin perasaan aku sama kamu, masalah diterima atau ngga itu tergantung kamu nya, aku juga gak bakal berharap lebih kok",
     },
     {
       title: "",
-      desc: "Kalau Kamu Risih Bilang yahh, 
-        'I Have Crush On You' , 
-      Semisal kamu udah punya pacar maaf yaa karna udah confess ke kamu...mungkin segitu saja dari aku, 
-  hehe makasih yaa sudah mau dibaca.",
+      desc: "Kalau Kamu Risih Bilang yahh, 'I Have Crush On You' , Semisal kamu udah punya pacar maaf yaa karna udah confess ke kamu...mungkin segitu saja dari aku, hehe makasih yaa sudah mau dibaca.",
     },
     {
       title: "I Have Crush On You!!",
